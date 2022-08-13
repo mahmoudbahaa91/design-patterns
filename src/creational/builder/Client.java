1aa6f2e5225d7c27956d60c45048ee2d5d5bb9c5
Mahmoud Bahaa <mahmoud.bahaa.fci@gmail.com>
2022-08-13 14:01:36 +0200

    adding creational/builder pattern

---
+++ src/creational/builder/Client.java
@@ -0,0 +1,10 @@
+package creational.builder;
+
+public class Client {
+
+    public static void main(String[] args) {
+        User user = User.UserBuilder("Mahmoud", "Youssef").withEmail("testEmail").withAge(30).build();
+        System.out.println(user);
+    }
+
+}
