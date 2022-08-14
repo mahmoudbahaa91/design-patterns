01ee5fa5968363ec3f9038049716c438ff8734ef
Mahmoud Bahaa <mahmoud.bahaa.fci@gmail.com>
2022-08-14 14:58:19 +0200

    adding creational/simplefactory pattern

---
+++ src/creational/simplefactory/PostFactory.java
@@ -0,0 +1,13 @@
+package creational.simplefactory;
+
+public class PostFactory {
+
+    public static Post createPost(String type) {
+        return switch (type) {
+            case "blog" -> new BlogPost();
+            case "news" -> new NewsPost();
+            default -> throw new IllegalArgumentException("Post type is unknown");
+        };
+    }
+
+}
